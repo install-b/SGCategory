cd1fdf183d40b4a398ac3f56d81ac4d1a219660b
ShangenZhang <645256685@qq.com>
2018-02-06 19:52:58 +0800

    init foundation category

---
+++ Classes/Foundation/SGFoundationCategore.h
@@ -0,0 +1,20 @@
+//
+//  SGFoundationCategory.h
+//  SGCategory
+//
+//  Created by Shangen Zhang on 2017/8/17.
+//  Copyright © 2017 Shangen Zhang. All rights reserved.
+//
+
+#ifndef SGFoundationCategory_h
+#define SGFoundationCategory_h
+
+#import "NSArray+SGArrayFormSet.h"
+#import "NSMutableArray+SGAddObject.h"
+#import "NSMutableDictionary+SGSetObjc.h"
+#import "NSObject+SGSelector.h"
+#import "NSString+SGHash.h"
+#import "NSString+SGRegex.h"
+#import "NSString+SGRemoveSprit.h"
+
+#endif /* SGFoundationCategory_h */
